571ff5bffa82935db2b36000a9fb02462e57302d
mozzi327 <mozzi327@gmail.com>
2023-04-15 20:07:03 +0900

    init: e-commerce Eureka

---
+++ user-service/src/main/java/com/example/userservice/ui/UserController.java
@@ -0,0 +1,17 @@
+package com.example.userservice.ui;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserController {
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+}
